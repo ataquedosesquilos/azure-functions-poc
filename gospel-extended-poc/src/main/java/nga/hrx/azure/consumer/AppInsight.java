9e9d07414866821faa08ddcb6bee12d0c1522264
joaoa <ataquedosesquilos@gmail.com>
2019-10-21 16:02:16 +0200

    add custom telemetry

---
+++ gospel-extended-poc/src/main/java/nga/hrx/azure/consumer/AppInsight.java
@@ -0,0 +1,35 @@
+package nga.hrx.azure.consumer;
+
+import java.util.Map;
+
+import com.microsoft.applicationinsights.TelemetryClient;
+import com.microsoft.applicationinsights.TelemetryConfiguration;
+import com.microsoft.applicationinsights.telemetry.Duration;
+
+public class AppInsight {
+	
+	private TelemetryClient myClient;
+	
+	public AppInsight() {
+		this.myClient = new TelemetryClient();
+	}
+	
+	public AppInsight(String instrumentationKey) {
+		TelemetryConfiguration conf = new TelemetryConfiguration() ;
+		conf.setInstrumentationKey(instrumentationKey);
+		this.myClient = new TelemetryClient(conf);
+	}
+	
+	public void trackMetric(String name, double value, Integer sampleCount, Double min, Double max, Double stdDev, Map<String, String> properties) {
+		this.myClient.trackMetric(name, value, sampleCount, min, max, stdDev, properties);
+	}
+	
+	public void trackMetric(String name, double value ) {
+		this.myClient.trackMetric(name, value);
+	}
+	
+	public void trackDependency(String dependencyName, String commandName, Long duration, boolean success) {
+		this.myClient.trackDependency(dependencyName, commandName, new Duration(duration), success);
+	}
+
+}
--- gospel-extended-poc/src/main/java/nga/hrx/gospel/consumer/Client.java
+++ gospel-extended-poc/src/main/java/nga/hrx/gospel/consumer/Client.java
@@ -8,13 +8,14 @@ import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.json.JSONObject;
 import com.microsoft.azure.functions.ExecutionContext;
 
-
+import nga.hrx.azure.consumer.AppInsight;
 import nga.hrx.utils.ApiException;
 import tech.gospel.sdk.model.Record;
 
 public class Client {
 	private final int GOSPEL_TIMEOUT = 120;
 	private GospelConsumer gospelConsumer;
+	private static AppInsight appInsight = new AppInsight();
 	
 	public Client(){
 		try {
@@ -26,7 +27,10 @@ public class Client {
 	
 	public Client(ExecutionContext context){
 		try {
+			long lStartTime = System.nanoTime();
 			this.gospelConsumer = new GospelConsumer();
+			 long lEndTime = System.nanoTime();
+			 Client.appInsight.trackMetric("Instantiate SDK",( lEndTime - lStartTime)/1000);
 		} catch ( Exception e) {
 			context.getLogger().log(Level.SEVERE, ExceptionUtils.getStackTrace(e), e);
 		}
@@ -34,7 +38,11 @@ public class Client {
 	
 	public void writeRecord( ArrayList<Record> record) throws ApiException {
 		try {
+			long lStartTime = System.nanoTime();
 			this.gospelConsumer.getSDK().createRecords(record).get(GOSPEL_TIMEOUT, TimeUnit.SECONDS);
+			 long lEndTime = System.nanoTime();
+			 Client.appInsight.trackMetric("write records via SDK", (lEndTime - lStartTime)/1000);
+			 Client.appInsight.trackDependency("WriteRecordToGospel", "HTTPPOST", lEndTime - lStartTime, true);
 		} catch ( Exception e) {
 			throw new ApiException( e);
 		}
@@ -42,7 +50,11 @@ public class Client {
 	
 	public   String readRecord(String id, String type) throws ApiException {
 		try {
+			long lStartTime = System.nanoTime();
 			Record record= this.gospelConsumer.getSDK().getRecord(type, id).get(GOSPEL_TIMEOUT, TimeUnit.SECONDS);
+			 long lEndTime = System.nanoTime();
+			 Client.appInsight.trackMetric("read record via SDK", (lEndTime - lStartTime)/1000);
+			 Client.appInsight.trackDependency("ReadRecordFromGospel", "HTTPGET", lEndTime - lStartTime, true);
 			JSONObject json = new JSONObject(record.getFields());
 			return json.toString();
 		} catch ( Exception e) {
